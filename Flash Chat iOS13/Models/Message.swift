175dda293adcd2fd2762390aa21cf16e286e0f88
Samed Torun <33091148+samedtorunn@users.noreply.github.com>
2022-08-05 11:06:23 +0300

    Add files via upload

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Sam on 28.06.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
